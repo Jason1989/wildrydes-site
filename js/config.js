5f87813ea33793c0e3914d16a62909dca17ecbc4
OhForReal <jianguo@smu.edu>
2020-10-21 14:50:43 -0700

    fix typo

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' us-east-1_FcHOG7fNT', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_FcHOG7fNT', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '317fpv9688eaf2161ke9ebtesm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
