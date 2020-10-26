80aa69d0347a16a8f3c01859b6f5c59da487e67a
OhForReal <jianguo@smu.edu>
2020-10-26 09:23:05 -0700

    fix typo

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://console.aws.amazon.com/apigateway/home?region=us-east-1#/apis/h8vt911rca/stages/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://h8vt911rca.execute-api.us-east-1.amazonaws.com/prod'
+        // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
